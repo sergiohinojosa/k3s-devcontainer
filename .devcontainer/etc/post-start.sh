472d873f25931e74cb77fda102a75e8fda7c42e6
Sergio Hinojosa <hj.sergio@gmail.com>
2025-03-20 19:03:37 +0000

    some deployment to show the issue

--- .devcontainer/etc/post-start.sh
+++ .devcontainer/etc/post-start.sh
@@ -5,3 +5,6 @@ helm install dynatrace-operator oci://public.ecr.aws/dynatrace/dynatrace-operato
 
 
 
+# Deploy a dynakube file and restart tomcat and it will have this issue
+
+kubectl create deployment kubernetes-bootcamp --image=gcr.io/google-samples/kubernetes-bootcamp:v1
