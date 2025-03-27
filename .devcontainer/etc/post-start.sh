0601c477af130f60a2f621bb9ebef03920276a58
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2025-03-27 16:38:19 +0000

    comit
    Signed-off-by: Sergio Hinojosa <sergio.hinojosa@dynatrace.com>

--- .devcontainer/etc/post-start.sh
+++ .devcontainer/etc/post-start.sh
@@ -6,5 +6,9 @@ helm install dynatrace-operator oci://public.ecr.aws/dynatrace/dynatrace-operato
 
 
 # Deploy a dynakube file and restart tomcat and it will have this issue
+#Node JS Version nok
+#kubectl create deployment kubernetes-bootcamp --image=gcr.io/google-samples/kubernetes-bootcamp:v1
 
-kubectl create deployment kubernetes-bootcamp --image=gcr.io/google-samples/kubernetes-bootcamp:v1
+kubectl run tomcat --image=tomcat:latest --port=8080 --dry-run=client -o yaml | kubectl apply -f -
+
+# curl -sS https://webinstall.dev/k9s | bash
